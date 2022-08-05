9189bc1b4e6662fbd9fa4550bdd4327026b973b4
rachata0212 <54613710+rachata0212@users.noreply.github.com>
2022-08-05 19:09:32 +0700

    Add files via upload

---
+++ Shareholder_System_2017/ShareHD/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ShareHD
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Frm_main());
+        }
+
+        static public SqlConnection CN;
+        static public string urldb = null;
+        static public string uid = null;
+        static public string uname = "";
+        static public int idmeeting = 0;
+        static public string namemeeting = "";
+        static public string datemeeting = "";
+        static public string id_registers="";
+
+    }
+}
