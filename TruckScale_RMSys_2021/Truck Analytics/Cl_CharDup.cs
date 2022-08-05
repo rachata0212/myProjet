d0c23e9a6353b80d3f074740a6416c89c43cdd49
Rachata.H <54613710+rachata0212@users.noreply.github.com>
2022-08-05 23:22:42 +0700

    Add files via upload

---
+++ TruckScale_RMSys_2021/Truck Analytics/Cl_CharDup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Truck_Analytics
+{
+    class Cl_CharDup
+    {
+        public static List<T> RemoveDuplicatesSet<T>(List<T> items)
+        {
+            // Use HashSet to remember items seen.
+            var result = new List<T>();
+            var set = new HashSet<T>();
+            for (int i = 0; i < items.Count; i++)
+            {
+                // Add if needed.
+                if (!set.Contains(items[i]))
+                {
+                    result.Add(items[i]);
+                    set.Add(items[i]);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ TruckScale_RMSys_2021/Truck Analytics/Class_Log.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Truck_Analytics
+{
+    class Class_Log
+    {
+        // Attibute
+        private string logdate = Program.Date_Now + ' ' + Program.Time_Now;
+        private string uname = Program.user_login;
+        private string macname = Program.MachineName;
+        private string ipaddress = Program.LocalIpAddress;
+
+        public string tbname;
+        public string oldvalue;
+        public string newvalue;
+        
+        
+
+
+        public void GetData(string tb_name, string old_value, string new_value, string mac_name, string ip_address)
+        {
+            this.tbname = tb_name;
+            this.oldvalue = old_value;
+            this.newvalue = new_value;
+            this.macname = mac_name;
+            this.ipaddress = ip_address;
+        }
+
+        public void Save_log()
+        {
+            SqlConnection con = new SqlConnection(Program.pathdb_Weight);
+            con.ConnectionString = Program.pathdb_Weight;
+            //SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+            con.Open();
+            string sql = "Insert Into [TB_Log] ([LogDateTime],[Username],[TableName],[OldValue],[NewValue],[MachineName],[LocalIpAddress]) Values('" + logdate + "', '" + uname + "', '" + tbname + "','" + oldvalue + "', '" + newvalue + "', '" + macname + "', '" + ipaddress + "')";
+            SqlCommand CM2 = new SqlCommand(sql, con);
+            SqlDataReader DR2 = CM2.ExecuteReader();
+            con.Close();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
